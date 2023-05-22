3c57714f7fedd3d1a2657966197c9f40a21ed68d
PaM <matar0236@gmail.com>
2023-05-22 09:55:57 +0000

    tache 8

--- src/main/java/com/garage/garage/controller/GarageController.java
+++ src/main/java/com/garage/garage/controller/GarageController.java
@@ -3,10 +3,12 @@ package com.garage.garage.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.garage.garage.model.Car;
@@ -31,31 +33,29 @@ public class GarageController {
     // Car mycar = new Car("lenang", "toyot", 2010, Car.Color.RED);
     // return mycar;
     // }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/cars")
+    @GetMapping("/cars")
     List<Car> getcars() {
         return garageService.getCars();
     }
 
-    @RequestMapping("/car/{id}")
+    @GetMapping("/car/{id}")
     Car getCar(@PathVariable long id) {
         return garageService.getCar(id);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/car/{id}")
+    @DeleteMapping("/car/{id}")
     void deleteCar(@PathVariable long id) {
 
         garageService.deleteCar(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/cars")
+    @PostMapping("/cars")
     public void addCar(@RequestBody Car car) {
 
         garageService.addCar(car);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/car/{id}")
-
+    @PutMapping("/car/{id}")
     public void updateCar(@RequestBody Car car, @PathVariable int id) {
         garageService.updateCar(car, id);
     }
